9261d405001fbda1579eeabf63eabb20afa8a62d
Jocelyn Chang <jocelync1094@gmail.com>
2019-10-11 20:52:20 -0700

    Creating a questions object, selecting elements for quiz container

---
+++ questions.js
@@ -0,0 +1,55 @@
+//my questions
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ______.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "What does HTML stand for?",
+        choices: ["Hyper Text Markup Language", "Hyperlinks and Text Markup Language", "Home Tool Markup Language", "None of the above"],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        title: "Inside which HTML element do we put the JavaScript Code?",
+        choices: ["<body>", "<javascript>", "<script>", "<div>"],
+        answer: "<script>"
+    },
+  ];
+
+// Selecting our elements
+  var startBtn = document.querySelector("#start");
+  var allChoicesBtn = document.querySelector("#choices");
+  var questionID = document.querySelector("#question");
+  var aBtn = document.querySelector("#A")
+  var bBtn = document.querySelector("#B")
+  var cBtn = document.querySelector("#C")
+  var dBtn = document.querySelector("#D")
+  var choiceClass =document.querySelector(".choice");
+
+//start
+startBtn.addEventListener("click",quizBegins);
+
+function quizBegins (){
+    startBtn.style="display:none";
+    var questionsIndex = 0;
+}
+
+
+
+//Changing to the next question
+  allChoicesBtn.addEventListener("click", nextQuestion)
+
+  function nextQuestion (){
+
+  }
