0c0e9ed1679d167e0f56fc024ccfd3acc4971bdd
Jocelyn Chang <jocelync1094@gmail.com>
2019-10-12 18:42:05 -0700

    Adding scoring

--- questions.js
+++ questions.js
@@ -1,77 +1,79 @@
 //my questions
 var questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-        title: "Arrays in JavaScript can be used to store ______.",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "all of the above"
-    },
-    {
-        title: "What does HTML stand for?",
-        choices: ["Hyper Text Markup Language", "Hyperlinks and Text Markup Language", "Home Tool Markup Language", "None of the above"],
-        answer: "Hyper Text Markup Language"
-    },
-    {
-        title: "Inside which HTML element do we put the JavaScript Code?",
-        choices: ["<body>", "<javascript>", "<script>", "<div>"],
-        answer: "<script>"
-    },
-  ];
-  var questionNumber;
-  var totalTime = 15*questions.length;
-  var questionTimer =15;
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+      title: "Arrays in JavaScript can be used to store ______.",
+      choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+      answer: "all of the above"
+  },
+  {
+      title: "What does HTML stand for?",
+      choices: ["Hyper Text Markup Language", "Hyperlinks and Text Markup Language", "Home Tool Markup Language", "None of the above"],
+      answer: "Hyper Text Markup Language"
+  },
+  {
+      title: "Inside which HTML element do we put the JavaScript Code?",
+      choices: ["<body>", "<javascript>", "<script>", "<div>"],
+      answer: "<script>"
+  },
+];
+var questionNumber;
+var totalTime = 15*questions.length;
+var questionTimer =15;
+var score = 75;
+
 // Selecting our elements
-  var startBtn = document.querySelector("#start");
-  var quizContainer=document.querySelector("#quiz-container")
-  var allChoicesBtn = document.querySelector("#choices");
-  var questionID = document.querySelector("#question");
-  var aBtn = document.querySelector("#A")
-  var bBtn = document.querySelector("#B")
-  var cBtn = document.querySelector("#C")
-  var dBtn = document.querySelector("#D")
-  var choiceClass =document.querySelector(".choice");
-  var timer = document.querySelector("#count-down-timer");
+var startBtn = document.querySelector("#start");
+var quizContainer=document.querySelector("#quiz-container")
+var allChoicesBtn = document.querySelector("#choices");
+var questionID = document.querySelector("#question");
+var aBtn = document.querySelector("#A")
+var bBtn = document.querySelector("#B")
+var cBtn = document.querySelector("#C")
+var dBtn = document.querySelector("#D")
+var choiceClass =document.querySelector(".choice");
+var timer = document.querySelector("#count-down-timer");
 
 //start
 startBtn.addEventListener("click",quizBegins);
 
 function quizBegins (){
-    startBtn.style="display:none";
-    quizContainer.style="display:block";
-    questionNumber = 0;
-    timerCountDown();
-    insertQuestionChoices(questionNumber);
-    questionCountDown();
+  startBtn.style="display:none";
+  quizContainer.style="display:block";
+  questionNumber = 0;
+  timerCountDown();
+  insertQuestionChoices(questionNumber);
+  questionCountDown();
 }
 
 function timerCountDown (){
-    var timerInterval = setInterval(function(){
-        timer.innerHTML= "<div>" + "Timer: " + totalTime +"</div>";
-        totalTime--;
+  var timerInterval = setInterval(function(){
+      timer.innerHTML= "<div>" + "Timer: " + totalTime +"</div>";
+      totalTime--;
 
-        if(totalTime===0){
-        clearInterval(timerInterval);
-        }
-        
-    },1000);
+      if(totalTime===0){
+      clearInterval(timerInterval);
+      }
+      
+  },1000);
 }
 
 
 function insertQuestionChoices (questionNumber) {
-    questionID.textContent=questions[questionNumber].title;
-    aBtn.textContent=questions[questionNumber].choices[0];
-    bBtn.textContent=questions[questionNumber].choices[1];
-    cBtn.textContent=questions[questionNumber].choices[2];
-    dBtn.textContent=questions[questionNumber].choices[3];  
+  questionID.textContent=questions[questionNumber].title;
+  aBtn.textContent=questions[questionNumber].choices[0];
+  bBtn.textContent=questions[questionNumber].choices[1];
+  cBtn.textContent=questions[questionNumber].choices[2];
+  dBtn.textContent=questions[questionNumber].choices[3];  
 }
 
 
@@ -79,29 +81,68 @@ function insertQuestionChoices (questionNumber) {
 
 var questionInterval;
 
-allChoicesBtn.addEventListener("click", nextQuestion)
+allChoicesBtn.addEventListener("click", function (e){
+  console.log("e is: ", e.path[0].id)
+  checkAnswers(e);
+  nextQuestion();
+})
 
-  function nextQuestion (){
-    questionNumber++;
+function nextQuestion (){
+    
+
+  questionNumber++;
+  if(questionNumber<questions.length){
     insertQuestionChoices(questionNumber);
-    if(questionNumber===questions.length-1){
-        questionNumber=0;
-    }
+
+    console.log(questionNumber);
     questionTimer=15;
     questionCountDown();
     clearInterval(questionInterval);
-
+    return;
+  } else {
+    quizContainer.style ="display:none";
+    
   }
+  
+
+}
 
 //Changing question by time
 function questionCountDown() {
-    questionInterval = setInterval(function(){
-        questionTimer--;
+  questionInterval = setInterval(function(){
+      questionTimer--;
 
-        if(questionTimer===0){
-        nextQuestion();
-        clearInterval(questionInterval);
-        }
-        
-    },1000);
+      if(questionTimer===0){
+      score-=15;
+      console.log(score);
+      nextQuestion();
+      clearInterval(questionInterval);
+      }
+      
+  },1000);
+}
+
+//answercheck
+function checkAnswers(event) {
+  var userBtn = event.path[0].id;
+  var userChoice;
+  if (userBtn === "A"){
+    userChoice = aBtn.textContent;
+  } else if (userBtn === "B"){
+    userChoice = bBtn.textContent; 
+  } else if (userBtn === "C"){
+    userChoice = cBtn.textContent;
+  } else if (userBtn === "D"){
+    userChoice = dBtn.textContent;
+  }
+  
+  if (userChoice !== questions[questionNumber].answer){
+    score -= 15;
+    totalTime-=15;
+    console.log("This is wrong");
+    console.log(score);
+  }else {
+    console.log("This is correct");
+    console.log(score);
+  }
 }
